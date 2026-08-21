bf9fde90197705b2e5d83d8f4565431e44f7b556
Tefera <tefealagaw@example.com>
2026-08-21 12:06:39 -0700

    add EmailJS credentials for contact form

--- portfolio/frontend/src/components/Contact.jsx
+++ portfolio/frontend/src/components/Contact.jsx
@@ -3,9 +3,9 @@ import useReveal from '../hooks/useReveal'
 import emailjs from '@emailjs/browser'
 import './Contact.css'
 
-const SERVICE_ID = 'YOUR_SERVICE_ID'
-const TEMPLATE_ID = 'YOUR_TEMPLATE_ID'
-const PUBLIC_KEY = 'YOUR_PUBLIC_KEY'
+const SERVICE_ID = 'service_mxbrabw'
+const TEMPLATE_ID = 'template_dgbnlnu'
+const PUBLIC_KEY = 'btRYYCTpE3W-gK2Uj'
 
 function Contact() {
   const [form, setForm] = useState({ name: '', email: '', message: '' })
